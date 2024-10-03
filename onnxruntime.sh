f27590a6d32b6078ca764a2a0bc56a8d10baa3c7
Andry Rakotozafindrabe <arakotoz@cern.ch>
2024-10-03 14:12:32 +0200

    fix onnx compilation

--- onnxruntime.sh
+++ onnxruntime.sh
@@ -1,6 +1,6 @@
 package: ONNXRuntime
 version: "%(tag_basename)s"
-tag: v1.18.1
+tag: v1.19.2
 source: https://github.com/microsoft/onnxruntime
 requires:
   - protobuf
@@ -20,25 +20,38 @@ prepend_path:
 
 mkdir -p $INSTALLROOT
 
+case $ARCHITECTURE in
+  osx*)
+    # If we preferred system tools, we need to make sure we can pick them up.
+    [[ -z $BOOST_ROOT ]] && BOOST_ROOT=$(brew --prefix boost)
+    [[ -z $PROTOBUF_ROOT ]] && PROTOBUF_ROOT=$(brew --prefix protobuf)
+#    [[ -z $PROTOBUF_ROOT ]] && PROTOBUF_ROOT=$(dirname "$(dirname "$(which protoc)")")
+    [[ ! -d $BOOST_ROOT ]] && unset BOOST_ROOT
+    [[ ! -d $PROTOBUF_ROOT ]] && unset PROTOBUF_ROOT
+    SONAME=dylib
+    MACOSX_RPATH=OFF
+  ;;
+  *) SONAME=so ;;
+esac
+
 cmake "$SOURCEDIR/cmake"                                                              \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                                             \
       -DCMAKE_BUILD_TYPE=Release                                                      \
       -DCMAKE_INSTALL_LIBDIR=lib                                                      \
       -DPYTHON_EXECUTABLE=$(python3 -c "import sys; print(sys.executable)")           \
-      -Donnxruntime_BUILD_UNIT_TESTS=OFF                                              \
-      -Donnxruntime_PREFER_SYSTEM_LIB=ON                                              \
+      ${MACOSX_RPATH:+-DMACOSX_RPATH=${MACOSX_RPATH}}                                 \
+      -DONNX_USE_PROTOBUF_SHARED_LIBS=ON                                              \
       -Donnxruntime_BUILD_SHARED_LIB=ON                                               \
-      -DProtobuf_USE_STATIC_LIBS=OFF                                                  \
-      -DUSE_PROTOBUF_SHARED_LIBS=ON                                                  \
-      -DONNX_BUILD_SHARED_LIBS=ON                                                    \
-      ${PROTOBUF_ROOT:+-DProtobuf_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf.dylib}           \
-      ${PROTOBUF_ROOT:+-DProtobuf_LITE_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf-lite.dylib} \
-      ${PROTOBUF_ROOT:+-DProtobuf_PROTOC_LIBRARY=$PROTOBUF_ROOT/lib/libprotoc.dylib}      \
-      ${PROTOBUF_ROOT:+-DProtobuf_INCLUDE_DIR=$PROTOBUF_ROOT/include}                 \
-      ${PROTOBUF_ROOT:+-DProtobuf_PROTOC_EXECUTABLE=$PROTOBUF_ROOT/bin/protoc}        \
-      ${RE2_ROOT:+-DRE2_INCLUDE_DIR=${RE2_ROOT}/include}                              \
-      ${FLATBUFFERS_ROOT:+-DFLATBUFFERS_INCLUDE_DIR=${FLATBUFFERS_ROOT}/include}      \
-      ${BOOST_ROOT:+-DBOOST_INCLUDE_DIR=${BOOST_ROOT}/include}                        \
+      -Donnxruntime_BUILD_UNIT_TESTS=OFF                                              \
+      -DONNX_BUILD_SHARED_LIBS=ON                                                     \
+      ${PROTOBUF_ROOT:+-DProtobuf_LIBRARY="$PROTOBUF_ROOT/lib/libprotobuf.$SONAME"}                 \
+      ${PROTOBUF_ROOT:+-DProtobuf_LITE_LIBRARY="$PROTOBUF_ROOT/lib/libprotobuf-lite.$SONAME"}       \
+      ${PROTOBUF_ROOT:+-DProtobuf_PROTOC_LIBRARY="$PROTOBUF_ROOT/lib/libprotoc.$SONAME"}            \
+      ${PROTOBUF_ROOT:+-DProtobuf_INCLUDE_DIR="$PROTOBUF_ROOT/include"}                             \
+      ${PROTOBUF_ROOT:+-DProtobuf_PROTOC_EXECUTABLE="$PROTOBUF_ROOT/bin/protoc"}                    \
+      ${RE2_ROOT:+-DRE2_ROOT="$RE2_ROOT"}                                                           \
+      ${FLATBUFFERS_ROOT:+-DFLATBUFFERS_INCLUDE_DIR=${FLATBUFFERS_ROOT}/include}                    \
+      ${BOOST_ROOT:+-DBOOST_INCLUDE_DIR=${BOOST_ROOT}/include}                                      \
       -DCMAKE_CXX_FLAGS="$CXXFLAGS -Wno-unknown-warning -Wno-unknown-warning-option -Wno-error=unused-but-set-variable -Wno-error=deprecated" \
       -DCMAKE_C_FLAGS="$CFLAGS -Wno-unknown-warning -Wno-unknown-warning-option -Wno-error=unused-but-set-variable -Wno-error=deprecated"
 
