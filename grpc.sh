f9fe985609c6fbfc5f6cee610216e9512e970bb7
Andry Rakotozafindrabe <arakotoz@cern.ch>
2024-10-15 21:52:19 +0200

    update grpc

--- grpc.sh
+++ grpc.sh
@@ -1,15 +1,15 @@
 package: grpc
 version: "%(tag_basename)s"
-tag: v1.50.1
+tag: v1.67.0
 requires:
   - protobuf
   - c-ares
   - "OpenSSL:(?!osx)"
   - "GCC-Toolchain:(?!osx)"
   - re2
+  - abseil
 build_requires:
   - CMake
-  - abseil
   - ninja
 source: https://github.com/grpc/grpc
 incremental_recipe: |
@@ -23,9 +23,12 @@ case $ARCHITECTURE in
     SONAME=dylib
     [[ ! $OPENSSL_ROOT ]] && OPENSSL_ROOT=$(brew --prefix openssl@3)
     [[ ! $PROTOBUF_ROOT ]] && PROTOBUF_ROOT=$(brew --prefix protobuf)
+    [[ ! $CARES_ROOT ]] && CARES_ROOT=$(brew --prefix c-ares)
     # to avoid issues with rpath on mac
+    packages_path="$ABSEIL_ROOT;$CARES_ROOT;$PROTOBUF_ROOT"
     extra_cmake_variables="-DCMAKE_INSTALL_RPATH=$INSTALLROOT/lib \
     -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
+    -DCMAKE_PREFIX_PATH=${packages_path} 
     "
   ;;
 esac
@@ -36,22 +39,22 @@ echo "OPENSSL_REVISION: $OPENSSL_REVISION"
 cmake $SOURCEDIR                                    \
   -G Ninja 					                                \
   ${CXXSTD:+-DCMAKE_CXX_STANDARD=$CXXSTD}           \
+  -DCMAKE_CXX_STANDARD_REQUIRED=TRUE                \
   -DCMAKE_INSTALL_PREFIX=$INSTALLROOT               \
-  -DgRPC_PROTOBUF_PACKAGE_TYPE="CONFIG"             \
   -DgRPC_BUILD_TESTS=OFF                            \
   -DBUILD_SHARED_LIBS=ON                            \
+  -DCMAKE_BUILD_TYPE=Release                        \
+  -DgRPC_CARES_PROVIDER=package                     \
   -DgRPC_SSL_PROVIDER=package                       \
   -DgRPC_ZLIB_PROVIDER=package                      \
-  -DgRPC_GFLAGS_PROVIDER=package                    \
   -DgRPC_PROTOBUF_PROVIDER=package                  \
   -DgRPC_ABSL_PROVIDER=package                      \
-  -DgRPC_BENCHMARK_PROVIDER=package                 \
+  -DgRPC_RE2_PROVIDER=package                       \
   -DgRPC_BUILD_GRPC_CSHARP_PLUGIN=OFF               \
   -DgRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN=OFF          \
-  -DgRPC_BUILD_GRPC_PHP_PLUGIN=OFF 		    \
+  -DgRPC_BUILD_GRPC_PHP_PLUGIN=OFF 		              \
   -DgRPC_BUILD_GRPC_CPP_PLUGIN=ON                   \
   -DgRPC_BUILD_CSHARP_EXT=OFF                       \
-  -DgRPC_RE2_PROVIDER=package                       \
   ${OPENSSL_ROOT:+-DOPENSSL_ROOT_DIR=$OPENSSL_ROOT} \
   ${OPENSSL_ROOT:+-DOpenSSL_ROOT="$OPENSSL_ROOT"}   \
   ${OPENSSL_ROOT:+-DOPENSSL_INCLUDE_DIRS=$OPENSSL_ROOT/include} \
