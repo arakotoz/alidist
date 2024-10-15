5ce04ce39ff068e44e64c2e31f52187b2b830106
Andry Rakotozafindrabe <arakotoz@cern.ch>
2024-10-15 21:51:53 +0200

    update protobuf

--- protobuf.sh
+++ protobuf.sh
@@ -1,24 +1,31 @@
 package: protobuf
-version: v21.9
+version: v28.2
 source: https://github.com/protocolbuffers/protobuf
+requires:
+  - abseil
 build_requires:
   - CMake
   - "GCC-Toolchain:(?!osx)"
   - alibuild-recipe-tools
+  - ninja
 prepend_path:
   # The protobuf headers must match the protoc binary version, so prevent the
   # use of system headers by putting ours first in the path.
   PKG_CONFIG_PATH: "$PROTOBUF_ROOT/lib/pkgconfig"
 ---
 #!/bin/bash -e
-cmake "$SOURCEDIR/cmake"                  \
-    -DCMAKE_INSTALL_PREFIX="$INSTALLROOT" \
-    -Dprotobuf_BUILD_TESTS=NO             \
-    -Dprotobuf_MODULE_COMPATIBLE=YES      \
-    -Dprotobuf_BUILD_SHARED_LIBS=OFF      \
+cmake "$SOURCEDIR"                          \
+    -G 'Ninja'                              \
+    -DCMAKE_INSTALL_PREFIX="$INSTALLROOT"   \
+    -Dprotobuf_ABSL_PROVIDER=package        \
+    -DCMAKE_PREFIX_PATH="$ABSEIL_ROOT"      \
+    ${CXXSTD:+-DCMAKE_CXX_STANDARD=$CXXSTD} \
+    -Dprotobuf_BUILD_TESTS=NO               \
+    -Dprotobuf_MODULE_COMPATIBLE=YES        \
+    -Dprotobuf_BUILD_SHARED_LIBS=OFF        \
     -DCMAKE_INSTALL_LIBDIR=lib
-make ${JOBS:+-j $JOBS}
-make install
+
+cmake --build . -- ${JOBS:+-j$JOBS} install
 
 mkdir -p "$INSTALLROOT/etc/modulefiles"
 alibuild-generate-module --bin --lib > "$INSTALLROOT/etc/modulefiles/$PKGNAME"
